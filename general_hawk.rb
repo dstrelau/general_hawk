3afeda4545bd67d413fb2ab5855fba69fa2021e9
Dean Strelau <dean@mintdigital.com>
2009-08-31 18:12:53 -0400

    Allow setting cijoe git config options within yaml.

--- general_hawk.rb
+++ general_hawk.rb
@@ -4,16 +4,30 @@ require 'yaml'
 class GeneralHawk
   class << self
     def to_app
-      joes = Rack::Builder.new
-      config.each do |app, opts|
-        joes.map("/#{app}") { run Class.new(CIJoe::Server).set(opts) }
-      end
-      joes.to_app
+      write_git_config
+      app
     end
 
     private
-    def config
-      @_config ||= YAML.load_file(File.dirname(__FILE__)+'/joes.yml')
+    def write_git_config
+      joes.each do |name, opts|
+        cijoes = opts.delete('cijoe')
+        cijoes.each do |key,value|
+          `cd #{opts['project_path']} && git config cijoe.#{key} #{value}`
+        end if cijoes.respond_to?(:each)
+      end
+    end
+
+    def app
+      builder = Rack::Builder.new
+      joes.each do |app, opts|
+        builder.map("/#{app}") { run Class.new(CIJoe::Server).set(opts) }
+      end
+      builder.to_app
+    end
+
+    def joes
+      @_joes ||= YAML.load_file(File.dirname(__FILE__)+'/joes.yml')
     end
   end
 end
